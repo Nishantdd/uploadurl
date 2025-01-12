65438552d461d8e9cc2842971091e2a98a008f90
Nishantdd <nishant.dahiya2000@gmail.com>
2025-01-12 16:48:53 +0530

    Add initial environment variable config implementation

---
+++ backend/config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+	Redis    RedisConfig
+	AWS      AWSConfig
+}
+
+type ServerConfig struct {
+	Address string
+}
+
+type PostgresConfig struct {
+	URI string
+}
+
+type RedisConfig struct {
+	Address  string
+	Password string
+}
+
+type AWSConfig struct {
+	S3BucketName string
+	Region       string
+	AccessKey    string
+	SecretKey    string
+}
+
+func Load() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return &Config{
+		Server: ServerConfig{
+			Address: os.Getenv("SERVER_ADDRESS"),
+		},
+		Postgres: PostgresConfig{
+			URI: os.Getenv("DATABASE_URL"),
+		},
+		Redis: RedisConfig{
+			Address:  os.Getenv("REDIS_ADDRESS"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+		},
+		AWS: AWSConfig{
+			S3BucketName: os.Getenv("S3_BUCKET"),
+			Region:       os.Getenv("AWS_REGION"),
+			AccessKey:    os.Getenv("AWS_ACCESS_KEY"),
+			SecretKey:    os.Getenv("AWS_SECRET_KEY"),
+		},
+	}
+}
