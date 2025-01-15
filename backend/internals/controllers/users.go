4cae144afb7f4ca58dd43c23f699dc18190def00
Nishantdd <nishant.dahiya2000@gmail.com>
2025-01-16 00:23:44 +0530

    Add UserWithPassword for requests that require password not to be hidden

--- backend/internals/controllers/users.go
+++ backend/internals/controllers/users.go
@@ -17,40 +17,35 @@ func GetUsers(c *gin.Context) {
 		return
 	}
 
-	for i := range users {
-		users[i].Password = ""
-	}
 	c.JSON(http.StatusOK, gin.H{"users": users})
 }
 
 func GetUserByID(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient data: ID is required"})
-		return
-	}
 
 	if err := database.DB.First(&user, id).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
 
-	user.Password = "" // prevention from sending password
 	c.JSON(http.StatusOK, gin.H{"user": user})
 }
 
 func CreateUser(c *gin.Context) {
+	var userReq models.UserWithPassword
 	var user models.User
 
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err := c.ShouldBindJSON(&userReq); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	user.Username = userReq.Username
+	user.Email = userReq.Email
 
 	// Hashing password
 	var err error
-	user.Password, err = utils.HashPassword(user.Password)
+	user.Password, err = utils.HashPassword(userReq.Password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 		return
@@ -68,11 +63,8 @@ func CreateUser(c *gin.Context) {
 
 func UpdateUser(c *gin.Context) {
 	var user models.User
+	var userReq models.UserWithPassword
 	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient data: ID is required"})
-		return
-	}
 
 	// Check if user exists
 	if err := database.DB.First(&user, id).Error; err != nil {
@@ -81,14 +73,16 @@ func UpdateUser(c *gin.Context) {
 	}
 
 	// Bind update data
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err := c.ShouldBindJSON(&userReq); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	user.Email = userReq.Email
+	user.Username = userReq.Username
 
 	// Hashing password
 	var err error
-	user.Password, err = utils.HashPassword(user.Password)
+	user.Password, err = utils.HashPassword(userReq.Password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 		return
@@ -101,16 +95,11 @@ func UpdateUser(c *gin.Context) {
 		return
 	}
 
-	user.Password = "" // prevention from sending password
 	c.JSON(http.StatusOK, gin.H{"user": user})
 }
 
 func DeleteUser(c *gin.Context) {
 	id := c.Param("id")
-	if id == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient data: ID is required"})
-		return
-	}
 
 	result := database.DB.Delete(&models.User{}, id)
 	if result.Error != nil {
--- backend/internals/models/users.go
+++ backend/internals/models/users.go
@@ -3,9 +3,15 @@ package models
 type User struct {
 	Model
 	Username string  `gorm:"type:varchar(100);unique;not null" json:"username" binding:"required,max=100"`
-	Email    string  `gorm:"type:varchar(50);unique;not null" json:"email" binding:"required,max=50"`
-	Password string  `gorm:"type:varchar(100);not null" json:"password" binding:"required,min=8,max=30"`
+	Email    string  `gorm:"type:varchar(50);unique;not null" json:"email" binding:"required,email,max=50"`
+	Password string  `gorm:"type:varchar(100);not null" json:"-" binding:"required,min=8,max=30"`
 	URLs     []Url   `gorm:"foreignKey:UserId" json:"urls,omitempty"`  // One-to-Many
 	Files    []Files `gorm:"foreignKey:UserId" json:"files,omitempty"` // One-to-Many
 	Slugs    []Slug  `gorm:"foreignKey:UserId" json:"slugs,omitempty"` // One-to-Many
+}
+
+type UserWithPassword struct {
+	Username string `json:"username" binding:"required,max=100"`
+	Email    string `json:"email" binding:"required,email,max=50"`
+	Password string `json:"password" binding:"required,min=8,max=30"`
 }
